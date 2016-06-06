a2ac0dee241e434ce42d47d132dfe6b2488e1604
R3t51W <laphwng@gmail.com>
2016-06-05 23:04:21 -0400

    rspec installed

--- market_api/config/routes.rb
+++ market_api/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   #Api definition
-  namespace :api, defaults: {format: :json} do
+  namespace :api, defaults: {format: :json},
     constraints: { subdomain: 'api' }, path: '/'  do
 
       scope module: :v1 do
