9ab874823222c717f9f20dc5cadca24433b32b68
R3t51W <laphwng@gmail.com>
2016-06-13 22:39:58 -0400

    create user endpoint created, rspec test added

--- market_api/spec/controllers/api/v1/users_controller_spec.rb
+++ market_api/spec/controllers/api/v1/users_controller_spec.rb
@@ -16,4 +16,37 @@ describe Api::V1::UsersController do
 
     it {should respond_with 200}
   end
+
+  describe "POST #create" do
+
+    context "when successfully created" do
+      before(:each) do
+        @user_attributes = FactoryGirl.attributes_for :user
+        post :create, {user: @user_attributes}, format: :json
+      end
+      it "renders json representation for user record just created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql @user_attributes[:email]
+      end
+      it {should respond_with 201}
+    end
+    context "When user is not created" do
+      before(:each) do
+        # no email included in test case
+        @invalid_user_attributes = {password: "12345678",
+                                    password_confirmation: "12345678"}
+        post :create, {user: @invalid_user_attributes}, format: :json
+      end
+      it "renders errors json" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+      it "renders JSON errors on why use could not be created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "can't be blank"
+      end
+      it {should respond_with 422}
+    end
+  end
+
 end
