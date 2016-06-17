501ff37cbc409ce2cc50a0b43d30a24d072c8a43
R3t51W <laphwng@gmail.com>
2016-06-16 23:58:49 -0400

    auth_token spec created

--- market_api/app/models/user.rb
+++ market_api/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :auth_token, uniqueness: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
