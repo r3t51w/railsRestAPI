e26bb1510cebc76493faeaab50d28fcba39e8429
R3t51W <laphwng@gmail.com>
2016-06-21 23:09:16 -0400

    Sessions controller added def create

--- market_api/config/routes.rb
+++ market_api/config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
                   constraints: ApiConstraints.new(version:1, default: true) do
           # We are giong to list our resources here
           resources :users, :only => [:show, :create, :update, :destroy]
+          resources :sessions, :only =>[:create, :destroy]
       end
 
 
